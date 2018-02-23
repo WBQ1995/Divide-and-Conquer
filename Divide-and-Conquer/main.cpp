f86a8f00871168eaef52e294d845aa2b9c6d9eb4
Your Name <315214192@qqq.com>
2018-02-22 21:06:03 -0500

    Initial Commit

---
+++ Divide-and-Conquer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Divide-and-Conquer
+//
+//  Created by PRO on 2018-02-22.
+//  Copyright © 2018 AIR. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
